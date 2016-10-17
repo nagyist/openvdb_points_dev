03162522e39699a5e6a4c8717c6308953d626596
Dan Bailey <dan@dneg.com>
2016-10-16 18:37:19 -0700

    Update copyright notices from Double Negative to Dreamworks Animation

--- openvdb_houdini/houdini/SOP_OpenVDB_Points_Group.cc
+++ openvdb_houdini/houdini/SOP_OpenVDB_Points_Group.cc
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2015-2016 Double Negative Visual Effects
+// Copyright (c) 2012-2016 DreamWorks Animation LLC
 //
 // All rights reserved. This software is distributed under the
 // Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
@@ -8,8 +8,8 @@
 // Redistributions of source code must retain the above copyright
 // and license notice and the following restrictions and disclaimer.
 //
-// *     Neither the name of Double Negative Visual Effects nor the names
-// of its contributors may be used to endorse or promote products derived
+// *     Neither the name of DreamWorks Animation nor the names of
+// its contributors may be used to endorse or promote products derived
 // from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -860,6 +860,6 @@ SOP_OpenVDB_Points_Group::removeViewportMetadata(PointDataGrid& outputGrid)
 
 ////////////////////////////////////////
 
-// Copyright (c) 2015-2016 Double Negative Visual Effects
+// Copyright (c) 2012-2016 DreamWorks Animation LLC
 // All rights reserved. This software is distributed under the
 // Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
